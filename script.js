6674a9873e84913bef666f0dc5aa76244190e15c
Mike Sasiela <m5455y@gmail.com>
2020-12-15 00:19:39 -0600

    Working on 5 day forecast.

--- script.js
+++ script.js
@@ -36,12 +36,11 @@ $.ajax({
     // Transfer Content to HTML
     // Adds Weather Icon next to City Name
     const dayZeroIcon = (`http://openweathermap.org/img/w/${response.weather[0].icon}.png`)
-    $(".city").html("<h4>" + response.name + (`<i><img src="${dayZeroIcon}" alt="Weather Icon"></i>`) + "</h4>");
-    //$(".city").html(`<h4>${response.name}<i><img src="${dayZeroIcon}" alt="Weather Icon"></i></h4>`);
-    $(".tempF").html("Temp: " + tempF.toFixed(2) + '<span>&#176;</span>');
-    $(".feelsLike").html("Feels Like: " + tempFFeelsLike.toFixed(0) + '<span>&#176;</span>');
-    $(".wind").text("Wind Speed: " + response.wind.speed + " mph");
-    $(".humidity").text("Humidity: " + response.main.humidity + "%");
+    $(".city").html("<h4>" + response.name + (`<i><img src="${dayZeroIcon}" alt="Weather Icon"></i>`) + "</h4>")
+    $(".tempF").html("Temp: " + tempF.toFixed(2) + '<span>&#176; F</span>');
+    $(".feelsLike").html("Feels Like: " + tempFFeelsLike.toFixed(0) + '<span>&#176;</span>')
+    $(".wind").text("Wind Speed: " + response.wind.speed + " mph")
+    $(".humidity").text("Humidity: " + response.main.humidity + "%")
 
   });
 
@@ -57,18 +56,64 @@ function searchCityForecast (input) {
         url: queryURL,
         method: "GET"
       }).then(function(response) {
-        console.log(response);
+        console.log(response)
 
-        const dayOneBlock = response.list[2];
-        // console.log(dayOneBlock);
-        const dayTwoBlock = response.list[10];
-        // console.log(dayTwoBlock);
-        const dayThreeBlock = response.list[18];
-        // console.log(dayThreeBlock);
-        const dayFourBlock = response.list[26];
-        // console.log(dayFourBlock);
-        const dayFiveBlock = response.list[34];
-        // console.log(dayFiveBlock);
+        // $('#dayOne').epmty()
+        // $('#dayTwo').epmty()
+        // $('#dayThree').epmty()
+        // $('#dayFour').epmty()
+        // $('#dayFive').epmty()
+
+        // Set forecast section of for each day to variables
+        var dayOneBlock = response.list[2]
+        var dayTwoBlock = response.list[10]
+        var dayThreeBlock = response.list[18]
+        var dayFourBlock = response.list[26]
+        var dayFiveBlock = response.list[34]
+
+        // Date to display on each forecast day
+        var dateOneText = moment(dayOneBlock.dt_txt).format("M/D")
+        var dateTwoText = moment(dayTwoBlock.dt_txt).format("M/D")
+        var dateThreeText = moment(dayThreeBlock.dt_txt).format("M/D")
+        var dateFourText = moment(dayFourBlock.dt_txt).format("M/D")
+        var dateFiveText = moment(dayFiveBlock.dt_txt).format("M/D")
+
+        // Setting weather icon's for each forecast day to a variable
+        var dayOneIcon = (`http://openweathermap.org/img/w/${dayOneBlock.weather[0].icon}.png`)
+        var dayTwoIcon = (`http://openweathermap.org/img/w/${dayTwoBlock.weather[0].icon}.png`)
+        var dayThreeIcon = (`http://openweathermap.org/img/w/${dayThreeBlock.weather[0].icon}.png`)
+        var dayFourIcon = (`http://openweathermap.org/img/w/${dayFourBlock.weather[0].icon}.png`)
+        var dayFiveIcon = (`http://openweathermap.org/img/w/${dayFiveBlock.weather[0].icon}.png`)
+
+        // Day 1 forecast card info
+        $('#dayOne').append("<h5>" + dateOneText + "</h5>")
+        $('#dayOne').append(`<i><img src="${dayOneIcon}" alt="Weather Icon"></i>`)
+        $('#dayOne').append("<p>Temp: " + (dayOneBlock.main.temp).toFixed(0) + " &deg;F</p>")
+        $('#dayOne').append("<p>Humidity: " + dayOneBlock.main.humidity + "%</p>")
+
+        // Day 2 forecast card info
+        $('#dayTwo').append("<h5>" + dateTwoText + "</h5>")
+        $('#dayTwo').append(`<i><img src="${dayTwoIcon}" alt="Weather Icon"></i>`)
+        $('#dayTwo').append("<p>Temp: " + (dayTwoBlock.main.temp).toFixed(0) + " &deg;F</p>")
+        $('#dayTwo').append("<p>Humidity: " + dayTwoBlock.main.humidity + "%</p>")
+
+        // Day 3 forecast card info
+        $('#dayThree').append("<h5>" + dateThreeText + "</h5>")
+        $('#dayThree').append(`<i><img src="${dayThreeIcon}" alt="Weather Icon"></i>`)
+        $('#dayThree').append("<p>Temp: " + (dayThreeBlock.main.temp).toFixed(0) + " &deg;F</p>")
+        $('#dayThree').append("<p>Humidity: " + dayThreeBlock.main.humidity + "%</p>")
+
+        // Day 4 forecast card info
+        $('#dayFour').append("<h5>" + dateFourText + "</h5>")
+        $('#dayFour').append(`<i><img src="${dayFourIcon}" alt="Weather Icon"></i>`)
+        $('#dayFour').append("<p>Temp: " + (dayFourBlock.main.temp).toFixed(0) + " &deg;F</p>")
+        $('#dayFour').append("<p>Humidity: " + dayFourBlock.main.humidity + "%</p>")
+
+        // Day 5 forecast card info
+        $('#dayFive').append("<h5>" + dateFiveText + "</h5>")
+        $('#dayFive').append(`<i><img src="${dayFiveIcon}" alt="Weather Icon"></i>`)
+        $('#dayFive').append("<p>Temp: " + (dayFiveBlock.main.temp).toFixed(0) + " &deg;F</p>")
+        $('#dayFive').append("<p>Humidity: " + dayFiveBlock.main.humidity + "%</p>")
 
       });
 
@@ -89,7 +134,7 @@ $(".search-button").on("click", function(event) {
     localStorage.setItem("cities", JSON.stringify(cityList))
     console.log(cityList)
 
-    // Running the searchCity function(passing in the artist as an argument)
+    // Running the searchCity function(passing in the city input as an argument)
     searchCityWeather(searchInput)
     searchCityForecast(searchInput)
 })
@@ -105,6 +150,7 @@ $('.search-input').keypress(function(e){
 $(document).on('click', '.list-group-item', function(){
     var listedCity = $(this).text()
     console.log(listedCity)
+
     searchCityWeather(listedCity)
     searchCityForecast(listedCity)
 })
